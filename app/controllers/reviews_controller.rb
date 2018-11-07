8d34d093b3e5f87d1e3b56714ff14750e32c4d6e
Lucien George <luciengeorge95@gmail.com>
2018-11-07 13:01:04 +0000

    add new review done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,22 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
 
+  def create
+    @review = Review.new(reviews_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reviews_params
+    params.require(:review).permit(:content, :rating)
   end
 end
